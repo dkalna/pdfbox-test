36e9f5e7c7faee02002dbe26213f81e9cb29436d
Dalibor Kalna <dalibor.kalna@dvbern.ch>
2022-02-23 14:36:06 +0100

    corrected according pdfbox example 'AddMessageToEachPage'

--- src/main/java/PdfboxTest.java
+++ src/main/java/PdfboxTest.java
@@ -2,11 +2,11 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.util.Matrix;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.UUID;
 
 public class PdfboxTest {
@@ -15,7 +15,7 @@ public class PdfboxTest {
         PdfboxTest pdfboxTest = new PdfboxTest();
         pdfboxTest.rotate();
         Thread.sleep(1000);
-        pdfboxTest.print();
+        pdfboxTest.print("TestApp" + "   " + UUID.randomUUID() + " 1 / 1");
     }
 
     private void rotate() throws Exception {
@@ -27,16 +27,7 @@ public class PdfboxTest {
             PDRectangle mediaBox = pdPage.getMediaBox();
 
             if(mediaBox.getWidth() > mediaBox.getHeight()) {
-                PDPageContentStream contents = new PDPageContentStream(
-                        pdDocument, pdPage, PDPageContentStream.AppendMode.APPEND, true, true);
-
-                float width = pdPage.getMediaBox().getHeight();
-                float height = pdPage.getMediaBox().getWidth();
-                contents.transform(Matrix.getRotateInstance(Math.toRadians(270), 0, width));
-
                 pdPage.setRotation(270);
-
-                contents.close();
             }
         }
 
@@ -44,29 +35,44 @@ public class PdfboxTest {
         pdDocument.close();
     }
 
-    private void print() throws Exception {
+    private void print(String message) throws Exception {
         PDDocument pdDocument = PDDocument.load(new File("simple_rotated.pdf"));
 
-        for(int pageIndex=0; pageIndex<pdDocument.getNumberOfPages(); pageIndex++) {
+        PDFont font = PDType1Font.HELVETICA;
+        float fontSize = 6.0f;
 
-            PDPage pdPage = pdDocument.getPage(pageIndex);
-            PDPageContentStream contents = new PDPageContentStream(
-                    pdDocument, pdPage, PDPageContentStream.AppendMode.APPEND, true, true);
+        for( PDPage pdPage : pdDocument.getPages()) {
 
-            applyPrintableInfoToPage(pdPage, contents);
-            contents.close();
+            PDRectangle pageSize = pdPage.getMediaBox();
+            float stringWidth = font.getStringWidth( message )*fontSize/1000f;
+
+            // calculate to center of the page
+            int rotation = pdPage.getRotation();
+            boolean rotate = rotation == 90 || rotation == 270;
+
+            float pageWidth = rotate ? pageSize.getHeight() : pageSize.getWidth();
+            float pageHeight = rotate ? pageSize.getWidth() : pageSize.getHeight();
+            float centerX = rotate ? pageHeight/2f : (pageWidth - stringWidth)/2f;
+            float centerY = rotate ? (pageWidth - stringWidth)/2f : pageHeight/2f;
+
+            // append the content to the existing stream
+            try (PDPageContentStream contentStream = new PDPageContentStream(pdDocument, pdPage, PDPageContentStream.AppendMode.APPEND, true, true))
+            {
+                contentStream.beginText();
+                // set font and font size
+                contentStream.setFont( font, fontSize );
+                if (rotate) {
+                    // rotate the text according to the page rotation
+                    contentStream.setTextMatrix(Matrix.getTranslateInstance(centerX, pageWidth - 20));
+                } else {
+                    contentStream.setTextMatrix(Matrix.getRotateInstance(Math.PI / 2, 20, centerY));
+                }
+                contentStream.showText(message);
+                contentStream.endText();
+            }
         }
 
         pdDocument.save(new File("simple_rotated_markers.pdf"));
         pdDocument.close();
-    }
-
-    private void applyPrintableInfoToPage(PDPage pdPage, PDPageContentStream contentStream) throws IOException {
-
-        contentStream.setFont(PDType1Font.HELVETICA, (float) 6);
-        contentStream.beginText();
-        contentStream.setTextMatrix(Matrix.getRotateInstance(Math.PI / 2, 20, pdPage.getMediaBox().getHeight() / 2 - 25));
-        contentStream.showText("TestApp" + "   " + UUID.randomUUID() + " 1 / 1");
-        contentStream.endText();
     }
 }
